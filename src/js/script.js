ceb62fb7a6be66e97e99904c8e6c6e49dc7650cb
Andriy Khimich <andrykhimich@gmail.com.noreply.github.com>
2021-03-03 15:48:11 +0200

    fixes

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,6 @@ const mainWrapper = $('.main-wrapper');
 const testimonialsSlider = $('#testimonialsSlider');
 const meditationSlider = $('#meditationSlider');
 const experianceSlider = $('#experianceSlider');
-// const featuresEventsSlider = $('#featuresEventsSlider');
-// const upcomingEventsSlider = $('#upcomingEventsSlider');
-
-
 const storeSlider = $('#storeSlider');
 const blogSlider = $('#blogSlider');
 const menuBtn = $('#menuBtn');
@@ -14,29 +10,7 @@ const nav = $('#nav');
 const partners = $('#partnerSlider');
 const aboutSlider = $('#aboutSlider');
 const helpSlider = $('#helpSlider');
-
-// const fixed = $('#target');
-
-
-// const show = document.querySelector('#nav');
-
-// console.log(partners);
-
-
-// const caseSlider = document.querySelector('#caseSlider');
-// const blogSlider = document.querySelector('#blogSlider');
-// const practiceSlider = document.querySelector('#practiceSlider');
-// const sidebarAccordion = document.querySelector('#sidebarAccordion');
-// const navItems = document.querySelectorAll('.nav-list a');
-// const submenu = document.querySelectorAll('.nav-sublist');
-// const moreLink = document.querySelectorAll('.js-open-text');
-// const hiddenText = document.querySelectorAll('.more-text');
-// const filterLink = document.querySelectorAll('.filter__list li');
-// const header = document.querySelector('.header');
-// const homeHeader = document.querySelector('.header_home');
-// const innerHeader = document.querySelector('.header_inner');
-// const logoImg = document.querySelector('.logo img');
-// const logoSource = document.querySelector('.logo source');
+const bookPartners = $('#partners');
 
 function initAboutSlider() {
   $(aboutSlider).slick({
@@ -176,98 +150,9 @@ document.addEventListener("DOMContentLoaded", function () {
       openMenu();
     }
   });
-
-  // const slider = document.querySelector('.help-slider');
-  // if (slider) {
-  //   let isDown = false;
-  //   let startX;
-  //   let scrollLeft;
-
-  //   slider.addEventListener('mousedown', (e) => {
-  //     isDown = true;
-  //     slider.classList.add('active');
-  //     startX = e.pageX - slider.offsetLeft;
-  //     scrollLeft = slider.scrollLeft;
-  //   });
-  //   slider.addEventListener('mouseleave', () => {
-  //     isDown = false;
-  //     slider.classList.remove('active');
-  //   });
-  //   slider.addEventListener('mouseup', () => {
-  //     isDown = false;
-  //     slider.classList.remove('active');
-  //   });
-  //   slider.addEventListener('mousemove', (e) => {
-  //     if (!isDown) return;
-  //     e.preventDefault();
-  //     const x = e.pageX - slider.offsetLeft;
-  //     const walk = (x - startX) * 3; //scroll-fast
-  //     slider.scrollLeft = scrollLeft - walk;
-  //     // console.log(walk);
-  //   });
-
-  // }
 });
 
-// function fixedImage() {
-//   const stopElement = $('#stopFixed');
-//   // console.log(fixed.offset().top);
-//   let topPosition = fixed.offset().top + 20;
-//   let stopPosition = stopElement.offset().top - fixed.height() - 40;
-//   // console.log(topPosition, stopPosition);
-//   let lastScrollTop = 0;
-//   $(window).scroll(function (event) {
-//     let scrollPosition = $(this).scrollTop();
-//     if (scrollPosition > lastScrollTop) {
-//       // console.log('Down');
-//       if (scrollPosition >= topPosition && scrollPosition <= stopPosition) {
-//         fixed.css({
-//           'position': 'fixed',
-//           'top': '0'
-//         });
-//       } else if (scrollPosition > stopPosition) {
-//         fixed.css({
-//           'position': 'absolute',
-//           'bottom': '0',
-//           'top': 'auto'
-//         });
-//       } else {
-//         fixed.css({
-//           'position': 'absolute',
-//           'top': '0'
-//         });
-//       }
-
-//     } else {
-//       // console.log('Up');
-
-//       if (scrollPosition >= topPosition && scrollPosition <= stopPosition) {
-//         fixed.css({
-//           'position': 'fixed',
-//           'top': '0',
-//           'bottom': 'auto'
-//         });
-//       } else if (scrollPosition > stopPosition) {
-//         fixed.css({
-//           'position': 'absolute',
-//           'bottom': '0',
-//           'top': 'auto'
-//         });
-//       } else {
-//         fixed.css({
-//           'position': 'absolute',
-//           'top': '0',
-//           'bottom': 'auto'
-//         });
-//       }
-//     }
-//     lastScrollTop = scrollPosition;
-//   });
-// }
-
-
 function initExperianceSlider() {
-
   $(experianceSlider).slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -285,121 +170,6 @@ function initExperianceSlider() {
     pauseOnHover: false
   });
 }
-
-
-// function initFeaturesEventsSlider() {
-//   // const counter = document.querySelector('#experianceSliderInfo');
-//   // $(experianceSlider).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-//   //   if (!slick.$dots) {
-//   //     return;
-//   //   }
-//   //   let i = (currentSlide ? currentSlide : 0) + 1;
-//   //   counter.innerHTML = '<span class="slider__number">' + i + '</span>' + '/' + (slick.$dots[0].children.length);
-//   // });
-
-//   $(featuresEventsSlider).slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     // dots: true,
-//     infinite: false,
-//     // speed: 800,
-//     fade: false,
-//     arrows: true,
-//     prevArrow: $('#featuresPrev'),
-//     nextArrow: $('#featuresNext')
-//     // responsive: [{
-//     //   breakpoint: 991,
-//     //   settings: {
-//     //     adaptiveHeight: true
-//     //   }
-//     // }]
-//   });
-// }
-
-// function initUpcomingEventsSlider() {
-//   // const counter = document.querySelector('#experianceSliderInfo');
-//   // $(experianceSlider).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-//   //   if (!slick.$dots) {
-//   //     return;
-//   //   }
-//   //   let i = (currentSlide ? currentSlide : 0) + 1;
-//   //   counter.innerHTML = '<span class="slider__number">' + i + '</span>' + '/' + (slick.$dots[0].children.length);
-//   // });
-
-//   $(upcomingEventsSlider).slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     // dots: true,
-//     infinite: false,
-//     // speed: 800,
-//     fade: false,
-//     arrows: true,
-//     prevArrow: $('#upcomingPrev'),
-//     nextArrow: $('#upcomingNext')
-//     // responsive: [{
-//     //   breakpoint: 991,
-//     //   settings: {
-//     //     adaptiveHeight: true
-//     //   }
-//     // }]
-//   });
-// }
-// function initPracticeSlider() {
-//   const counter = document.querySelector('#practiceSliderInfo');
-//   $(practiceSlider).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-//     if (!slick.$dots) {
-//       return;
-//     }
-//     let i = (currentSlide ? currentSlide : 0) + 1;
-//     counter.innerHTML = '<span class="slider__number">' + i + '</span>' + '/' + (slick.$dots[0].children.length);
-//   });
-
-//   $('#practiceSlider').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     dots: true,
-//     infinite: false,
-//     speed: 800,
-//     fade: true,
-//     arrows: true,
-//     prevArrow: $('#ptacticePrev'),
-//     nextArrow: $('#ptacticeNext'),
-//     responsive: [{
-//       breakpoint: 575,
-//       settings: {
-//         adaptiveHeight: true
-//       }
-//     }]
-//   });
-// }
-
-// function initCaseSlider() {
-//   const counter = document.querySelector('#caseSliderInfo');
-//   $(caseSlider).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-//     if (!slick.$dots) {
-//       return;
-//     }
-//     let i = (currentSlide ? currentSlide : 0) + 1;
-//     counter.innerHTML = '<span class="slider__number">' + i + '</span>' + '/' + (slick.$dots[0].children.length);
-//   });
-
-//   $('#caseSlider').slick({
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     dots: true,
-//     infinite: false,
-//     speed: 1000,
-//     arrows: true,
-//     prevArrow: $('#casePrev'),
-//     nextArrow: $('#caseNext'),
-//     responsive: [{
-//       breakpoint: 991,
-//       settings: {
-//         slidesToShow: 1
-//       }
-//     }]
-//   });
-// }
 
 svg4everybody();
 
@@ -426,48 +196,21 @@ $(document).ready(function () {
   if (experianceSlider.length) {
     initExperianceSlider();
   }
-
-  // if (featuresEventsSlider.length) {
-  //   initFeaturesEventsSlider();
-  // }
-
-  // if (upcomingEventsSlider.length) {
-  //   initUpcomingEventsSlider();
-  // }
   if (partners.length) {
-    // console.log('lenght1')
     initPartnersSlider();
+  }
+  if (bookPartners.length) {
+    initBookPartnersSlider();
   }
   if (aboutSlider.length) {
     initAboutSlider();
-
   }
   initTestimonialsSlider();
-  // initBannerSlider();
   initMeditationSlider();
-
   initStoreSlider();
   initHelpSlider();
   initBlogSlider();
-  // if (fixed.length && ($(window).width() > 1199)) {
-  //   fixedImage();
-  // }
-  // showOnLoad();
   showOnScroll($(window).scrollTop());
-  $(window).scroll(function () {
-    const scrollValue = $(this).scrollTop();
-    showOnScroll(scrollValue);
-    scrollValue >= 1 ? closeMenu() : null;
-  });
-
-
-
-  $(window).resize(function () {
-    if (partners.length) {
-      // console.log('lenght')
-      initPartnersSlider();
-    }
-  });
 
   function initPartnersSlider() {
     // console.log('start')
@@ -511,9 +254,74 @@ $(document).ready(function () {
       });
     }
   }
+  // SET POST DESCRIPTION LIMIT //
+  $('.book-testimonials__card').each(function () {
+    const limit = 110;
+    let post = $(this);
+    let text = post.find('.book-testimonials__text p');
+    console.log(text.height());
+    let link = $('<span></span>').text('read more').addClass('read-more');
+
+    if (text.height() > limit) {
+
+      text.css({
+        'height': limit,
+        'overflow': 'hidden'
+      });
+
+      post.append(link);
+    }
+    link.click(function () {
+      let trimText = $(this).parent().find('.book-testimonials__text p');
+      if (text.height() <= limit) {
+        trimText.css({
+          'height': 'auto',
+          'overflow': 'visible'
+        });
+        $(this).text('read less')
+
+      } else {
+        text.css({
+          'height': limit,
+          'overflow': 'hidden'
+        });
+        $(this).text('read more')
+      }
+    });
+  });
+  $(window).scroll(function () {
+    const scrollValue = $(this).scrollTop();
+    showOnScroll(scrollValue);
+    scrollValue >= 1 ? closeMenu() : null;
+  });
+
+  $(window).resize(function () {
+    if (partners.length) {
+      // console.log('lenght')
+      initPartnersSlider();
+    }
+  });
 });
 
-
+function initBookPartnersSlider() {
+  bookPartners.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplaySpeed: 0,
+    speed: 6000,
+    cssEase: 'linear',
+    responsive: [{
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 2,
+      }
+    }]
+  });
+}
 
 function showOnScroll(scrollValue) {
   $('.js-scroll').each(function () {
